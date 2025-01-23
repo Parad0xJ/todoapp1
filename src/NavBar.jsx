64524e303f23942c4cdffbc902eadab42dd4fa3e
Parad0xJ <laurent-lyon@live.fr>
2025-01-24 00:17:03 +0100

    MAJ navbar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line react/prop-types
-//Ajout du . devant /src/ des 2 images por github pages
+import bootstrapLogo from "./assets/bootstrap-logo.svg";
+import reactLogo from "./assets/react.svg";
+
 export default function NavBar({ width = 30, height = 24 }) {
   return (
     <nav className="navbar bg-success">
       <div className="container-fluid justify-content-center">
         <a className="navbar-brand" href="#">
           <img
-            src="../src/assets/bootstrap-logo.svg"
+            src={bootstrapLogo}
             alt="Logo"
             width={width}
             height={height}
@@ -15,7 +17,7 @@ export default function NavBar({ width = 30, height = 24 }) {
           React & Boostrap APP
         </a>
         <img
-          src="../src/assets/react.svg"
+          src={reactLogo}
           alt="Logo"
           width={width}
           height={height}
